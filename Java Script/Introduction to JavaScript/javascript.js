dc45c920f938e755c17a12f64b1e3b24db56bd34
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-14 01:33:10 +0530

    Intro to JS

---
+++ Java Script/Introduction to JavaScript/javascript.js
@@ -0,0 +1,13 @@
+   alert("Hello World");
+   console.log("Code is running...")
+   console.log("Code is running...")
+   console.log("Code is running...")
+   var a = prompt("Enter your number")
+   console.log("Your Number is"+a)
+   var isTrue = confirm("Are u sure u want too blast your computer??")
+   if(isTrue){
+    console.log("Your computer is blasted")
+   }
+   else{
+    console.log("Your computer is safe")
+   }
