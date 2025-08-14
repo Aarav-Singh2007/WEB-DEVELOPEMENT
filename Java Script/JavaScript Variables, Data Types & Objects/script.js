5342ed9f0d102370d22a859acd6e5701d2ff48c3
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-14 15:51:41 +0530

    Variables and object

---
+++ Java Script/JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,29 @@
+console.log("Hello world")
+let a = 5;
+let b = 6;
+let c = "Aarav"
+// console.log(a+b+c)
+// console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+let x = "Aarav SIngh";
+let y = 56;
+let z = 3.14;
+const p = true;
+let q = undefined;;
+let  r = null;
+console.log( x , y , z , p , q, r)
+console.log( typeof x , typeof y , typeof z , typeof p , typeof q, typeof r)
+
+let o ={
+    "name" : "Aarav Singh",
+    "Job role": "Web development",
+    "Job Salary" : 5600 
+
+}
+console.log(o)
+o.salary = "100cr"
+console.log(o)
