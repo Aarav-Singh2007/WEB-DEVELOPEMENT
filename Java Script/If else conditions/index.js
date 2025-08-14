6d69ffb0832553cc6e3adced205710c0dff207c9
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-14 22:25:33 +0530

    If Else if else Ladder

---
+++ Java Script/If else conditions/index.js
@@ -0,0 +1,17 @@
+console.log("Hello I am Conditional tutorial")
+
+let age = 5
+let grace = 4
+age+=grace
+console.log(age)
+
+if(age>18)
+    console.log("You are eligible to drive")
+else
+    console.log("not eligible")
+
+a = 6
+b = 8
+let c = a>b ? (a-b):(b-a)
+console.log(c)
+
