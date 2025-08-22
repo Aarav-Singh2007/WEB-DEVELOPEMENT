a6c21afcaa081c1447f74fa06236b9eb7f5b4a49
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-22 21:25:07 +0530

    Loops compleared

--- Java Script/JavaScript Loops/index.js
+++ Java Script/JavaScript Loops/index.js
@@ -1,5 +1,24 @@
 console.log("I am learning JavaScript loops")
 
-let a = 1
-for (let i = 0; i <100; i++)
-console.log(a+i)
+// let a = 1
+// for (let i = 0; i <100; i++)
+// console.log(a+i)
+// let obj ={
+//     name : "Aarav",
+//     role : "Programming",
+//     company:"Google"
+// }
+// for (const key in obj) {
+   
+//         const element = obj[key];
+//         console.log(key, element)
+        
+//     }
+// for (const c of "Aarav") {
+//     console.log(c)
+// }
+let i=9;
+while (i<6){
+    console.log(i)
+    i++
+}
