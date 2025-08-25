ddc1bc3da129201d0cd4a99d3b521798f7709e4a
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-25 22:11:26 +0530

    Strings

---
+++ Java Script/JavaScript Strings/index.js
@@ -0,0 +1,13 @@
+// console.log("this is strings tutorial")
+// let a = "Aarav"
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+
+// let real_name = "Aarav"
+// let friend = "Harry"
+// console.log(` His name is ${real_name } and his friends real_name is ${ friend } `)
+
+let b = "Shivan"
+console.log(b.replace("Sh", "77"))
+console.log(b.slice(1,5))
