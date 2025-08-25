3477a4e89c078b9f3a4bbd32343fa1a707959726
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-25 19:51:16 +0530

    Functions

---
+++ Java Script/Functions/index.js
@@ -0,0 +1,18 @@
+// function nice(name) {
+//     console.log("HEy" + name+ "you are nice")
+//     console.log("HEy" +name+ "you are good")
+//     console.log("HEy" +name+ "your tshirt is good too")
+//     console.log("HEy" +name+ "your code is good")
+// }
+// nice("Aarav")
+
+// function sum(a,b) {
+//     console.log(a+b)
+// }
+// sum(3,5)
+
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+func1(64);
+
