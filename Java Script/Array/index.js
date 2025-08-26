6b12b4b7ab29b53515d1a12cf68c89461e59525f
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-26 20:53:06 +0530

    Now I will be learning about loops

---
+++ Java Script/Array/index.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.toString())
+// console.log(arr.shift())
+// console.log(arr)
+// console.log(arr.push(100))
+// console.log(arr.pop())
+
+// let a1 =[1,2,3]
+// let a2 =[4,5,6]
+// let a3 =[9,8,7]
+
+// console.log(a1.concat(a2,a3))
+// console.log(a3.sort())
+// console.log(arr.splice(1,2,7.8))
+// console.log(arr)
