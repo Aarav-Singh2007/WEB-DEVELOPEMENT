4a601bb335846a29420da8c043b8a379c1921951
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-08-27 21:07:29 +0530

    Arays

--- Java Script/Array/mfr.js
+++ Java Script/Array/mfr.js
@@ -1,5 +1,15 @@
-let arr = [2, 4, 6, 8, 11]
-let newarr = arr.map((e) => {
-    return e ** 2
-})
-console.log(newarr)
+let arr = [2, 4, 6, 8, 111]
+// let newarr = arr.map((e) => {
+//     return e ** 2
+// })
+// console.log(newarr)
+
+
+const greaterthan7 = e => {
+              if (e>7){
+                return true
+              }
+              return false
+}
+
+console.log(arr.filter(greaterthan7))
