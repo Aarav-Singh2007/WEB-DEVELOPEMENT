74055663d682bfecfa2bc8f629be295738328722
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-09-06 13:53:12 +0530

    Advance Js

---
+++ Java Script/Advance Js/script.js
@@ -0,0 +1,24 @@
+// async function sleep() {
+//     return new Promise((resolve, reject )=>{
+//         setTimeout(() => {
+//             resolve(455)
+//         }, 2000);
+//     })
+// }
+//  let a = await sleep()
+//  let b = await sleep()
+function sum(a, b, c) {
+    return a + b + c}
+async function main() {
+            // let [x,y]= [1,5,7]
+            // console.log(x,y)
+
+            // let obj = {
+            //     a:1,
+            //     b:2,
+            //     c:3
+        }
+let arr = [1, 4, 7]
+console.log(sum(...arr))
+
+main()
